f3642f0794f8933f13418d8522ca74f6b64de389
Ganesh <sree.ganeshji@gmail.com>
2020-12-27 18:56:08 -0800

    Download data

--- awsAmplifyTest1/AmplifyHelper/AmplifyFileManager.swift
+++ awsAmplifyTest1/AmplifyHelper/AmplifyFileManager.swift
@@ -14,6 +14,7 @@ class AmplifyStorage{
     var progressSink : AnyCancellable?
     
     func uploadData(data:Data) {
+        //uploading data with an existing key will overwrite the previous data.
         print("Uploading ", String(data: data, encoding: .utf8))
         
         let storageOperation = Amplify.Storage.uploadData(key: "data stuff", data: data)
@@ -29,5 +30,22 @@ class AmplifyStorage{
         }, receiveValue: { (data:String) in
             print("Data is : ",data)
         })
+    }
+    
+    func downloadData(completion:@escaping (Data)->()) {
+        let storageOperation = Amplify.Storage.downloadData(key: "data stuff")
+        progressSink = storageOperation.progressPublisher.sink { (progress:Progress) in
+            print("Progress is \(progress)")
+        }
+        resultSink = storageOperation.resultPublisher.sink {
+            if case let .failure(StorageError) = $0{
+                print("Failed: \(StorageError.debugDescription) and \(StorageError.recoverySuggestion)")
+            }
+        } receiveValue: {
+            (data:Data) in
+            print("Data is \(data)")
+            completion(data)
+        }
+
     }
 }
--- awsAmplifyTest1/ContentView.swift
+++ awsAmplifyTest1/ContentView.swift
@@ -10,15 +10,44 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var data:DataModels
     @State var text:String = ""
+    @State var received:String = ""
     var body: some View {
-        VStack{
+        List{
             TextField("Write here", text: self.$text)
-            Text(self.text)
-            Button(action:{self.data.amplifyManager.uploadData(data: self.text.data(using: .utf8)!)}){
+            Text(self.received)
+            Button(action:upDownload){
                 Text("Upload")
+            }
+            Button(action:download){
+                Text("Download")
             }
         }
     }
+    
+    func upload(){
+        //upload
+        self.data.amplifyManager.uploadData(data: self.text.data(using: .utf8)!)
+    }
+    
+    func download(){
+        //download
+        self.data.amplifyManager.downloadData(completion: updateData)
+    }
+    
+    func upDownload(){
+
+        upload()
+        download()
+ 
+    }
+    
+    func updateData(data:Data){
+    
+            self.received = String(data: data, encoding: .utf8) ?? "Couldn't update"
+    
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
